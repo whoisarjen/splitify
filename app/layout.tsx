1ac7173a8fa00e9970576d752df967e78f46c54c
whoisarjen2 <157956920+whoisarjen2@users.noreply.github.com>
2024-02-04 20:47:14 +0000

    improve manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,10 @@ export const metadata = {
     apple: "/apple-touch-icon.png",
   },
   manifest: `${siteConfig.url}/site.webmanifest`,
+  generator: "Next.js",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
--- next.config.js
+++ next.config.js
@@ -34,8 +34,10 @@ const nextConfig = {
 }
 
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
 })
 
 const withContentlayer = createContentlayerPlugin({
