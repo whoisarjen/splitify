6b4fbf8164c6917238222e335fcfcff8a5627460
whoisarjen2 <157956920+whoisarjen2@users.noreply.github.com>
2024-02-04 20:33:13 +0000

    fix safe area

--- containers/FooterMenuMobile/FooterMenuMobile.tsx
+++ containers/FooterMenuMobile/FooterMenuMobile.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export const FooterMenuMobile = () => {
     return (
         <div className="md:hidden">
-            <div className="h-16 background-red-500 flex pb-safe" />
+            <div className="footer background-red-500 flex" />
             <div className="z-40 mt-0 bt-0 flex w-full justify-center bg-background/60 backdrop-blur-xl transition-all border-t fixed bottom-0 left-0 right-0 justify-evenly pb-safe">
                 {dashboardConfig.footerNav.filter(({ disabled }) => !disabled).slice(0, 5).map((item, index) => {
                     const Icon = Icons[item.icon || "arrowRight"]
@@ -15,7 +15,7 @@ export const FooterMenuMobile = () => {
                             <Link
                                 key={index}
                                 href={item.disabled ? "/" : item.href}
-                                className="flex-1 flex justify-center items-center flex-col h-16"
+                                className="flex-1 flex justify-center items-center flex-col footer"
                             >
                                 <Icon className="size-4" />
                                 <span className="text-xs">{item.title}</span>
